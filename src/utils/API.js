a3aaf64842fb34e6e4fb3c07955eb86bf4e8b8b9
Macz Norton <71162422+macz-norton@users.noreply.github.com>
2021-02-26 11:52:49 -0800

    Set up API file using axios

---
+++ src/utils/API.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+const BASEURL = "https://randomuser.me/api/";
+
+const API = {
+    getUsers: function(numberUsers) {
+        return axios.get(BASEURL + `?results=${numberUsers}`);
+    }
+};
+
+export default API;
