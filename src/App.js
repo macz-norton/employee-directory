3289700aa9eab6c1b8bbdf05cc98a53da6068a48
Macz Norton <71162422+macz-norton@users.noreply.github.com>
2021-02-26 11:55:43 -0800

    Added json to house test data and new UserContainer for API calls

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Body from "./components/Body";
 import Header from "./components/Header";
+// import testUsers from "./testUsers.json";
 
 class App extends React.Component {
   
