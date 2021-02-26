69a39cc28bece035d47e0b91b7914f4c2f2688c1
Macz Norton <71162422+macz-norton@users.noreply.github.com>
2021-02-26 11:31:54 -0800

    Added starting information for user and passed as deconstructed prop into Body.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+import React from "react";
 import Body from "./components/Body";
 import Header from "./components/Header";
 
-function App() {
-  return (
-    <div className="container">
-      <Header />
-      <Body />
-    </div>
-  );
+class App extends React.Component {
+  
+  state = {
+    users: []
+  }
+  
+  render() {
+    return (
+      <div className="container">
+        <Header />
+        <Body users={this.state.users} />
+      </div>
+    );
+  }
+
 }
 
 export default App;
