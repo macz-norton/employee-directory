e91d9a27a49abdada959f694a7ff047aa211682b
Macz Norton <71162422+macz-norton@users.noreply.github.com>
2021-03-01 14:13:35 -0800

    Separated out RandomUserContainer from App file

--- src/App.js
+++ src/App.js
@@ -1,50 +1,10 @@
 import React from "react";
-import Body from "./components/Body";
-import Filters from "./components/Filters";
-import Header from "./components/Header";
-import API from "./utils/API";
-// import testUsers from "./testUsers.json";
+import RandomUserContainer from "./components/RandomUserContainer"
 
-class App extends React.Component {
-  
-  state = {
-    users: [],
-    search: ""
-  }
 
-  componentDidMount() {
-    this.randomUser("");
-  }
+function App() {
 
-  randomUser = query => {
-    API.getUsers(query)
-      .then(users => this.setState({ users: users.data }))
-      .catch(err => console.log(err))
-  }
-
-  handleInputChange = event => {
-    const { name, value } = event.target;
-
-    this.setState({
-      [name]: value
-    });
-  };
-
-  // handleFormSubmit = event => {
-  //   event.preventDefault();
-  //   this.randomUser(this.state.search);
-  // }
-  
-  render() {
-    return (
-      <div className="container">
-        <Header />
-        <Filters handleInputChange={this.handleInputChange}/>
-        {this.state.search}
-        <Body users={this.state.users} search={this.state.search}/>
-      </div>
-    );
-  }
+  return <RandomUserContainer />;
 
 }
 
